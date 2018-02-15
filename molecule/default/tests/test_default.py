714a47ea9d2be6f8559b30eb07fd9716c07844d2
Andrey Gubarev <mylokin@me.com>
2018-02-15 10:11:21 +0300

    drop truthy rule

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,7 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_consul_configuration(host):
+    host.run_expect([0], 'consul validate /etc/consul.d')
